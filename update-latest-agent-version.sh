46bc8f48dfd0b50d69f8dee88f4c3738af8b695b
Kirsten Kluge <live@kirk.one>
2020-05-14 16:48:12 +0000

    Add auto version update

---
+++ update-latest-agent-version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+latest=$(cat ./latest-agent-version)
+
+shopt -s globstar
+for file in ./linux/**/releases; do
+    # overwrite target
+    echo "$latest" > $file
+    # appent to target
+    # echo "$latest" > $file
+done
+
+# Write the latest version number to the latest tag
+sed -i -E "s/[0-9]*\.[0-9]*\.[0-9]*/$latest/g" ./linux/latest.tag
