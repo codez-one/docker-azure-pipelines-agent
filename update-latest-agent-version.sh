464e041dcac27c380189c48fe4b679b43849a536
hangy <hangy@hangy.de>
2021-02-13 14:02:52 +0100

    build: Update the Windows agent version like the Linux version

--- update-latest-agent-version.sh
+++ update-latest-agent-version.sh
@@ -11,5 +11,12 @@ for file in ./linux/**/releases; do
     # echo "$latest" > $file
 done
 
+for file in ./windows/**/releases; do
+    # overwrite target
+    echo "$latest" > $file
+    # appent to target
+    # echo "$latest" > $file
+done
+
 # Write the latest version number to the latest tag
 sed -i -E "s/[0-9]*\.[0-9]*\.[0-9]*/$latest/g" ./linux/latest.tag
